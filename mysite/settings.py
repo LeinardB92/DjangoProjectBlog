0b37739cb06fe04e9c929d787b855153b33195a2
LeinardB92 <boanergespife@gmail.com>
2022-11-03 22:46:01 -0600

    Agregando 'leinardb92a.pythonanywhere.com'a ALLOWED_HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$g@k8rtxjc7$cgbu4&&ci+mbdet1z@m0()145-62606ttohgm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['leinardb92a.pythonanywhere.com']
 
 
 # Application definition

f58b44cbae91a5bcd69643190544daaf4b130f3b
LeinardB92 <boanergespife@gmail.com>
2022-11-03 21:50:12 -0600

    "Template post_list modificado para mostrar 'post' desde la base de datos."

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,11 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    # parece ser que al inicializar los atributos del modelo Post (y 
+    # cualquiera en general), es necesario indicar el tipo de dato con 
+    # las fuciones, como, .CharField, TextFiel, etc.
+    # Dentro de los métodos (como en este caso publish), ya no es necesario
+    # indicar el tipo de dato.
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -19,3 +24,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')

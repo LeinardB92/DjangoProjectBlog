c15dac5c0c5791487f715050ef8d56a38a3b238c
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2022-11-07 15:27:57 -0600

    Agregadas vistas y plantilla para el detalle del post del blog

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Posst
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
 
 def post_list(request):
     posts = Posst.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    # Aquí se está mandando a post a la plantilla para poder usar sus valores "{'postsss': posts}"
+    # Aquí se está mandando a post a la plantilla para poder usar sus valores -> "{'postsss': posts}"
     return render(request, 'blog/post_list.html', {'postsss': posts}) 
+
+def post_detail(request, pk):
+#    Posst.objects.get(pk=pk)
+    post = get_object_or_404(Posst, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
